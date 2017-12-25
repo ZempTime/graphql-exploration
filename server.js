6ac14b5d81ac23954c7564e6b92c3a6ff9d85752
Chris Zempel <zempel@efeqdev.com>
2017-12-25 17:11:48 -0600

    Working exploratory apollo graphql server that talks with sqlite, mongodb, and remote api

--- server.js
+++ server.js
@@ -1,13 +1,38 @@
 import express from 'express';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import compression from 'compression';
 import bodyParser from 'body-parser';
 import schema from './data/schema';
+import { Engine } from 'apollo-engine';
 
 const GRAPHQL_PORT = 3000;
+const ENGINE_API_KEY = process.env.APOLLO_ENGINE_API_KEY;
+
+const engine = new Engine({
+  engineConfig: {
+    apiKey: ENGINE_API_KEY,
+    stores: [
+      {
+        name: 'inMemEmbeddedCache',
+        inMemory: {
+          cacheSize: 20971520
+        }
+      }
+    ],
+    queryCache: {
+      publicFullQueryStore: 'inMemEmbeddedCache'
+    }
+  },
+  graphqlPort: GRAPHQL_PORT
+});
+
+engine.start();
 
 const graphQLServer = express();
+graphQLServer.use(engine.expressMiddleware());
+graphQLServer.use(compression());
 
-graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema, tracing: true, cacheControl: true }));
 graphQLServer.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
 graphQLServer.listen(GRAPHQL_PORT, () =>
